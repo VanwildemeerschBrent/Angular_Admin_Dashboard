cf8be1ab5f1acb755964c9729e2bc96b0f577062
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-10-02 14:21:05 +0200

    Form validation registration

--- src/app/authentication/registration/registration.component.ts
+++ src/app/authentication/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators, ValidationErrors, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/_models/user.model';
 
@@ -12,34 +12,33 @@ export class RegistrationComponent implements OnInit {
 
   user: User;
   registrationForm: FormGroup;
-  validationErrors: string[];
-  confirmedPassword: string = '';
-  constructor(private router: Router) { }
+
+  isSubmitted: boolean = false;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.user = new User();
-    this.registrationForm = new FormGroup({
-      userName: new FormControl(this.user.userName, [Validators.required, Validators.minLength(4)]),
-      userEmail: new FormControl(this.user.userEmail, [Validators.required]),
-      userPassword: new FormControl(this.user.userPassword, [Validators.required]),
-      userConfirmedPassword: new FormControl(this.confirmedPassword, [Validators.required])
+    this.registrationForm = this.formBuilder.group({
+      userName: new FormControl(this.user.userName, [Validators.required, Validators.minLength(2)]),
+      userEmail: new FormControl(this.user.userEmail, [Validators.required, Validators.email]),
+      userPassword: new FormControl(this.user.userPassword, [Validators.required, Validators.minLength(6)]),
+      userConfirmedPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
 
   get userName(): AbstractControl { return this.registrationForm.get('userName'); }
+
   get userEmail(): AbstractControl { return this.registrationForm.get('userEmail'); }
 
+  get userPassword(): AbstractControl { return this.registrationForm.get('userPassword'); }
+
+  get userPasswordConfirmed(): AbstractControl { return this.registrationForm.get('userConfirmedPassword'); }
+
   onRegister(): void {
-    this.validateInput();
-    console.warn(this.registrationForm.value)
+    this.isSubmitted = true;
+    if (this.registrationForm.valid) {
+      
+    } 
   }
-
-  validateInput(): void {
-
-
-  }
-
-
-
-
 }
