f31ffb1bd54d771711bdc8caf3275944f80bb844
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-10-05 17:21:30 +0200

    Footer & Header components

--- src/app/structure/footer/footer.component.ts
+++ src/app/structure/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  currentYear: Number = new Date().getFullYear();
   constructor() { }
 
   ngOnInit() {
