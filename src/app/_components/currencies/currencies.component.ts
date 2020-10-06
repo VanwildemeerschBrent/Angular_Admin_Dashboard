8476170fdbd169bcc185616c4b658fdfed187a72
Brent Vanwildemeersch <vanwildemeersch.brent@gmail.com>
2020-10-06 16:41:52 +0200

    Search function currency list

--- src/app/_components/currencies/currencies.component.ts
+++ src/app/_components/currencies/currencies.component.ts
@@ -12,18 +12,27 @@ export class CurrenciesComponent implements OnInit {
 
   currencies: Currencies[];
   currencyDetail: CurrencyDetail;
+  selectedCurrency: Currencies;
+  filteredCurrencies: Currencies[];
   constructor(private currencyService: CurrencyService) { }
 
   ngOnInit(): void {
     this.currencyService.getAllCurrencies().subscribe((currencies) => {
       this.currencies = currencies.sort((a, b) => (a.country < a.country) ? -1 : (a.country > b.country) ? 1 : 0);
+      this.filteredCurrencies = this.currencies;
     });
   }
 
   onClickCurrency(currency: Currencies): void {
+    this.selectedCurrency = currency;
     this.currencyService.getCurrencyData(currency.currency).subscribe((currencyDetail) => {
       this.currencyDetail = currencyDetail;
     });
+  }
+
+  onChangeSearchInput(event: KeyboardEvent) {
+    const input = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredCurrencies = this.currencies.filter(x => x.country.toLowerCase().includes(input));
   }
 
 }
